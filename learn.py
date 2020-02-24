f7a9f99bf2746346a72d71da17ccb1c8600491dd
Pranshu Mishra <pranshumshr.04@gmail.com>
2020-02-24 23:35:10 +0530

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,40 +1,49 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
-#Reading Data
+# Reading the dataset
 data = pd.read_csv('Boston.csv')
 X1 = data.iloc[:,1:5]
 X2 = data.iloc[:,6:14]
 X = pd.concat([X1,X2],axis=1)
 y = pd.DataFrame(data.iloc[:,14])
 
-#Data Preprocessing
-from sklearn import preprocessing
+# Preprocessing data
 X = preprocessing.normalize(X)
 
-#Splitting Data
+# Splitting the data into train set and test set
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.1)
 
 
+# Initialising the ANN
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Neural Network
-classifier = Sequential()
+'''
+model = pickle.load(open('model.sav', 'rb'))
+'''
 
-classifier.add(Dense(output_dim = 512, init = 'normal', activation = 'relu', input_dim = 12))
-classifier.add(Dense(output_dim = 128, init = 'normal', activation = 'relu'))
-classifier.add(Dense(output_dim = 1, init = 'normal', activation = 'linear'))
+#Skip this step if model.sav is already trained and loaded
+#Building and training the neural network
+model = Sequential()
+model.add(Dense(output_dim = 1024, init = 'normal', activation = 'relu', input_dim = 12))
+model.add(Dense(output_dim = 512, init = 'normal', activation = 'relu'))
+model.add(Dense(output_dim = 1, init = 'normal', activation = 'linear'))
+model.compile(optimizer = 'adam', loss = 'mse') 
+model.fit(X_train, y_train, batch_size = 5, nb_epoch = 200)
 
-classifier.compile(optimizer = 'adam', loss = 'mse') 
+#Saving the model
+import pickle
+pickle.dump(model, open('model.sav','wb'))
 
-classifier.fit(X_train, y_train, batch_size = 5, nb_epoch = 100)
+# Predicting
+y_pred = model.predict(X_test)
 
-y_pred1 = classifier.predict(X_test)
-
+#Model Evaluation
 from sklearn import metrics
-metrics.mean_absolute_error(y_test, y_pred1)
-metrics.explained_variance_score(y_test, y_pred1)
+metrics.mean_absolute_error(y_test, y_pred)
+metrics.explained_variance_score(y_test, y_pred)
